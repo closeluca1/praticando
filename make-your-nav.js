6d9f4c51d6db657689fa028e6fbb163e9c66154d
Patrick Lucas <57973233+closeluca1@users.noreply.github.com>
2020-08-15 14:23:56 -0300

    Create make-your-nav.js

---
+++ make-your-nav.js
@@ -0,0 +1,28 @@
+function changeliwidth(){
+    let liwidth = document.getElementById("li-width").value;
+    let changewidhth = document.querySelectorAll("li");
+    for (i = 0; i < changewidhth.length; i++)
+    changewidhth[i].style.width = liwidth;
+    document.getElementById("final-li-width").innerHTML = "width: " + liwidth + ";";
+};
+function changeliheight(){
+    let liheight = document.getElementById("li-height").value;
+    let changeheight = document.querySelectorAll("li");
+    for (i = 0; i < changeheight.length; i++)
+    changeheight[i].style.height = liheight;
+    document.getElementById("final-li-height").innerHTML = "height: " + liheight + ";";
+};
+function changelibg(){
+    let libg = document.getElementById("li-bg").value;
+    let changebg = document.querySelectorAll("li");
+    for (i = 0; i < changebg.length; i++)
+    changebg[i].style.background = libg;
+    document.getElementById("final-li-bg").innerHTML = "background: " + libg + ";";
+};
+function changeliborder(){
+    let liborder = document.getElementById("li-border").value;
+    let changeborder = document.querySelectorAll("li");
+    for (i = 0; i < changeborder.length; i++)
+    changeborder[i].style.borderColor = liborder;
+    document.getElementById("final-li-border").innerHTML = "border: 1px solid " + liborder + ";";
+};
